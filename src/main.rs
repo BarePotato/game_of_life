cec094fb06eb90ce1ef915180adb3dc365049031
Bare <bareknuckle@nah.com>
2020-07-31 17:08:06 -0400

    old changes never pushed

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,18 @@ use sfml::graphics::{
     Color, RectangleShape, RenderStates, RenderTarget, RenderWindow, Shape, Transformable,
 };
 use sfml::system::Vector2f;
-use sfml::window::{mouse, Event, Style};
+use sfml::window::{mouse, Event, Key, Style};
 
 use rand::{thread_rng, Rng};
 
 use std::time::{Duration, Instant};
 
+use std::thread;
+
 fn main() {
     // interface
-    let win_w = 1600;
-    let win_h = 900;
+    let win_w = 600;
+    let win_h = 600;
     let mut win = RenderWindow::new(
         (win_w, win_h),
         "Game of Life",
@@ -26,20 +28,22 @@ fn main() {
     let frame_rate = Duration::from_millis(333);
 
     // game states
-    let scale = 2; // defines grid size. 1 to smallest_dimension
-    let step = 10;
+    let scale = 4; // defines grid size. 1 to smallest_dimension
+    let step = 5;
     let mut tick = Duration::from_millis(100);
     let mut first_run = true;
     let mut timer = Instant::now();
     let mut generation = true;
+    let mut paused = false;
+    let mut do_step = false;
     let mut cell = RectangleShape::with_size(Vector2f::new(scale as f32, scale as f32));
-    cell.set_fill_color(&Color::WHITE);
+    cell.set_fill_color(&Color::rgb(137, 165, 150));
 
     // game
     let grid_w = win_w as usize / scale;
     let grid_h = win_h as usize / scale;
     let mut gen_last = vec![vec![0; grid_w]; grid_h];
-    let mut gen_next = gen_last.clone();//vec![vec![0; grid_w]; grid_h];
+    let mut gen_next = gen_last.clone(); //vec![vec![0; grid_w]; grid_h];
 
     fn gen_default(gen: &mut Vec<Vec<usize>>) {
         let mut rng = thread_rng();
@@ -72,6 +76,21 @@ fn main() {
                         tick -= Duration::from_millis(step);
                     }
                 }
+                Event::KeyPressed {
+                    code,
+                    alt,
+                    ctrl,
+                    shift,
+                    system,
+                } => {
+                    if code == Key::P {
+                        paused = !paused;
+                    } else if code == Key::N {
+                        do_step = true;
+                    } else if code == Key::Q || code == Key::S {
+                        win.close();
+                    }
+                }
                 _ => {}
             }
         }
@@ -85,55 +104,64 @@ fn main() {
             frame_timer = Instant::now();
         }
 
-        win.set_title(format!("Game of Life - {:?} - {} fps", tick, fps).as_str());
+        win.set_title(
+            format!(
+                "Game of Life - {:?} - {} fps - Paused: {}",
+                tick, fps, paused
+            )
+            .as_str(),
+        );
 
         if timer.elapsed() < tick && !first_run {
             continue;
         }
 
         // update grid
-        let mut gen_now = &mut gen_last;
-        let gen_future = if !generation && !first_run {
-            gen_now = &mut gen_next;
-            &mut gen_last
-        } else {
-            &mut gen_next
-        };
+        if !paused || do_step {
+            do_step = false;
+            let mut gen_now = &mut gen_last;
+            let gen_future = if !generation && !first_run {
+                gen_now = &mut gen_next;
+                &mut gen_last
+            } else {
+                &mut gen_next
+            };
 
-        for (y, row) in gen_future.iter_mut().enumerate() {
-            for (x, col) in row.iter_mut().enumerate() {
-                let mut count = 0;
-                for pos_y in -1..2 {
-                    for pos_x in -1..2 {
-                        let yy = (y as isize + grid_h as isize + pos_y) as usize % grid_h;
-                        let xx = (x as isize + grid_w as isize + pos_x) as usize % grid_w;
+            for (y, row) in gen_future.iter_mut().enumerate() {
+                for (x, col) in row.iter_mut().enumerate() {
+                    let mut count = 0;
+                    for pos_y in -1..2 {
+                        for pos_x in -1..2 {
+                            let yy = (y as isize + grid_h as isize + pos_y) as usize % grid_h;
+                            let xx = (x as isize + grid_w as isize + pos_x) as usize % grid_w;
 
-                        count += gen_now[yy][xx];
+                            count += gen_now[yy][xx];
+                        }
+                    }
+
+                    count -= gen_now[y][x];
+                    match gen_now[y][x] {
+                        0 if count == 3 => *col = 1,
+                        1 if count < 2 || count > 3 => *col = 0,
+                        1 if count == 2 || count == 3 => *col = 1,
+                        _ => *col = gen_now[y][x],
                     }
                 }
+            }
+            generation = !generation;
 
-                count -= gen_now[y][x];
-                match gen_now[y][x] {
-                    0 if count == 3 => *col = 1,
-                    1 if count < 2 || count > 3 => *col = 0,
-                    1 if count == 2 || count == 3 => *col = 1,
-                    _ => *col = gen_now[y][x],
+            win.clear(&Color::BLACK);
+            // draw grid
+            for (y, row) in gen_future.iter().enumerate() {
+                for (x, col) in row.iter().enumerate() {
+                    if *col == 1 {
+                        cell.set_position(((x * scale) as f32, (y * scale) as f32));
+                        win.draw_rectangle_shape(&cell, RenderStates::default());
+                    }
                 }
             }
+            win.display();
         }
-        generation = !generation;
-
-        win.clear(&Color::BLACK);
-        // draw grid
-        for (y, row) in gen_future.iter().enumerate() {
-            for (x, col) in row.iter().enumerate() {
-                if *col == 1 {
-                    cell.set_position(((x * scale) as f32, (y * scale) as f32));
-                    win.draw_rectangle_shape(&cell, RenderStates::default());
-                }
-            }
-        }
-        win.display();
 
         if first_run {
             first_run = false;
@@ -142,5 +170,7 @@ fn main() {
         timer = Instant::now();
 
         frames += 1;
+
+        thread::yield_now();
     }
 }
